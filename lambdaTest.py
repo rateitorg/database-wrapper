14dba7d006aef2b1f88d17efa70a907299c936b8
Jake Brunning <jakebrunning37@gmail.com>
2024-11-30 20:08:05 +0000

    set up database connect and flask setup

--- lambdaTest.py
+++ lambdaTest.py
@@ -1,19 +1,40 @@
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+
 import os
 
-#Intilise neo4j credentials. Constants from env file. Used to get information needed to connect to the database
-URI: str
-USERNAME:str
-PASSWORD:str
+#run a query
+def runQuery(query, parameters):
+    with driver.session() as session:
+        return session.run(query, parameters)
 
+# initilise the flask instance
+def initFlask():
+    global app
+    app = Flask = (__name__)
 
-def main():
-    print("Hello world")
+# initilise the connection to the database
+def initDatabaseConnection():
+    global driver # the access to the database
+    load_dotenv() # so we can get the data to connnect to the db
 
-if __name__ == "__main__":
-    load_dotenv()
+    # user data to connect
     URI = os.getenv("NEO4J_URI")
     USERNAME = os.getenv("NEO4J_USERNAME")
     PASSWORD = os.getenv("NEO4J_PASSWORD")
-    main()
+
+    # declare driver to connect to the database
+    driver = GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD))
+
+
+#  initilise the environment
+def initEnvironment():
+    initDatabaseConnection()
+    initFlask()
+    
+
+if __name__ == "__main__":
+    initEnvironment()
+
+
