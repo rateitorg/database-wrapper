a23d2fab7f7c072be9d005790f475c35600edca9
Jake Brunning <jakebrunning37@gmail.com>
2024-12-01 01:08:28 +0000

    changed so its working

--- lambdaTest.py
+++ lambdaTest.py
@@ -1,18 +1,22 @@
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
+import json
+import logging
 
 import os
 
+#initilise flask
+app = Flask(__name__)
+
+#initilise logger
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+
 #run a query
 def runQuery(query, parameters):
-    with driver.session() as session:
-        return session.run(query, parameters)
+    return driver.session().run(query, parameters)
 
-# initilise the flask instance
-def initFlask():
-    global app
-    app = Flask = (__name__)
 
 # initilise the connection to the database
 def initDatabaseConnection():
@@ -28,13 +32,26 @@ def initDatabaseConnection():
     driver = GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD))
 
 
-#  initilise the environment
-def initEnvironment():
-    initDatabaseConnection()
-    initFlask()
-    
+#get all data in the database
+@app.route('/get-all-data', methods=['POST'])
+def getAllData():
+    data = request.json
+    query = data.get("query")
+    parameters = data.get("parameters", {})
+
+    #get result. Throw error if fails
+    try:
+        result = runQuery(query, parameters)
+        return jsonify([record.data() for record in result])
+    except Exception as e:
+        return jsonify({"error":str(e)}), 500
+
+
+
+
 
 if __name__ == "__main__":
-    initEnvironment()
+    initDatabaseConnection() #connect to the database
+    app.run(debug=True)
 
 
