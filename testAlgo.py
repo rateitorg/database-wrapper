511f678f542bc121d6fe917162c984054ccac558
Jake-Brunning <tarmacshrimp@gmail.com>
2025-04-08 13:11:06 +0100

    changed error output and moved some functions

---
+++ testAlgo.py
@@ -0,0 +1,23 @@
+import sys
+
+
+#!/usr/bin/env python3
+
+"""
+testAlgo.py
+
+Description: testing the wikipedia tree maker
+
+Usage:
+    python testAlgo.py [arguments]
+
+Author: Jake Brunning
+"""
+
+
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
